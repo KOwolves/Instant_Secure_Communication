e25a4d8448b43bc867bdbe454af762f902bd9e67
SHAO Yinan <144402196+syn796@users.noreply.github.com>
2025-07-04 17:10:41 +0800

    Add files via upload

---
+++ server/config.py
@@ -0,0 +1,15 @@
+# 数据库信息
+db_info = {
+    "host": "127.0.0.1",
+    "port": 1433,
+    "user": "secure_chat_user",
+    "password": "SecureChat123!",
+    "database": "secure_communication",
+}
+
+# 日志文件名
+LOG_FILE = ''
+
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 50000
+BUFFER_SIZE = 4096
